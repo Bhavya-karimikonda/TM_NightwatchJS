04d8751b3abbd569d4048296493bd80a7a094c2f
Bhavya Karimikonda <bhavya.ka@browserstack.com>
2023-06-26 11:27:04 +0530

    Initial commit

---
+++ Nightwatch/tests/first_demo.js
@@ -0,0 +1,32 @@
+//demo.js is a JS file under tests folder
+module.exports = {
+    before : function(browser) {
+
+      //Declaring Global Timeout
+      browser.globals.waitForConditionTimeout = 5000;
+    },
+
+    'Wikipedia Search 1' : function (browser) {
+    browser
+      .url('https://www.wikipedia.org/')
+      .assert.title('Wikipedia')
+      .setValue('#searchInput', 'Google')
+      .click('button[type="submit"]')
+      .assert.containsText('h1#firstHeading', 'Google')
+      .assert.title('Google - Wikipedia')
+  },
+
+  'Duck Duck Go Search 1' : function (browser) {
+    browser
+      .url('https://duckduckgo.com/')
+      .assert.title('DuckDuckGo')
+      .setValue('#searchbox_input', 'Google')
+      .click('button[type="submit"]')
+      .assert.title('Google at DuckDuckGo')
+  },
+
+  after : function(browser) {
+    browser.end();
+  }
+
+}
